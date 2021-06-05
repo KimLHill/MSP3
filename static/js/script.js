c69440175aec681c43a2eda090da2204c35ebceb
KimLHill <kimberley@midwayelectronics.co.uk>
2021-06-05 21:28:32 +0000

    Added task collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,9 @@
+// Initialize Materialize features using jQuery
 $(document).ready(function(){
     // Initialize mobile collapse with custom right side functionality
     $(".sidenav").sidenav({edge: "right"});
+    // Initialize accordian collapsible functionality
+    $(document).ready(function(){
+      $('.collapsible').collapsible();
+    });
   });
